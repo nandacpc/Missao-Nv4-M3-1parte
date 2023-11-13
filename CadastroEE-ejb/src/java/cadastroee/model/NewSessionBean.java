ed443af0dec6219d5409b9a155b51ecb55bc7e33
nandacpc <nandacpc@gmail.com>
2023-11-13 05:24:59 -0300

    Missao nv4 m3

---
+++ CadastroEE-ejb/src/java/cadastroee/model/NewSessionBean.java
@@ -0,0 +1,10 @@
+package cadastroee.model;
+
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class NewSessionBean implements NewSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
